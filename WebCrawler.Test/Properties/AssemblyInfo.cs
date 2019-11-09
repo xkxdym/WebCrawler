d5cd8db12af3d56973ec97aac2d19ad567339679
xiaoxueling <xiaoxl084520@163.com>
2019-11-09 10:12:53 +0800

    add ico

--- WebCrawler.Test/Properties/AssemblyInfo.cs
+++ WebCrawler.Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("WebCrawler.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("图片下载工具")]
+[assembly: AssemblyDescription("分类深度挖掘网络图片")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WebCrawler.Test")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyCompany("梦工厂有限公司")]
+[assembly: AssemblyProduct("图片下载工具")]
+[assembly: AssemblyCopyright("Copyright © Dream 2018-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
